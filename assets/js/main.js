b0eec535b5f6d7859d673429de94669477330554
MatP25 <93398088+MatP25@users.noreply.github.com>
2022-10-03 17:14:58 -0300

    test1

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,20 +167,20 @@
     const getJsonData = async (url, hasFilter) => {
         const resp = await fetch(url);
 
-        console.log(resp);
+        // console.log(resp);
 
-        const data = await resp.text();
-        // const data = await resp.json();
+        // const data = await resp.text();
+        const data = await resp.json();
 
-        console.log(data);
+        // console.log(data);
 
-        // if (!hasFilter) {
-        //     createProducts(shop, data, cart);
-        // } else {
-        //     const filteredByName = nameFilter(data, searchBar.value.toLowerCase());
-        //     const multiFiltered = multipleFilters(filteredByName, selectCategory.value, maxPriceInput.value, minPriceInput.value);
-        //     createProducts(shop, multiFiltered, cart);
-        // }
+        if (!hasFilter) {
+            createProducts(shop, data, cart);
+        } else {
+            const filteredByName = nameFilter(data, searchBar.value.toLowerCase());
+            const multiFiltered = multipleFilters(filteredByName, selectCategory.value, maxPriceInput.value, minPriceInput.value);
+            createProducts(shop, multiFiltered, cart);
+        }
         
     }
     
